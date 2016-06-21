ac490e1c1babdf4a010d368208c0961361190b30
Chris-WF <Chris-WF@users.noreply.github.com>
2016-06-21 12:38:55 +0200

    Update Db.php

--- libraries/Db.php
+++ libraries/Db.php
@@ -1,15 +1,31 @@
 <?php
 
-function insertUser($db, $user, $pass, $dbName, $characterID, $corporationID, $allianceID, $groups, $authString, $active)
+function insertInviteCode($db, $user, $pass, $dbName, $fleetId, $inviteCode, $accessToken)
 {
-
     $conn = new mysqli($db, $user, $pass, $dbName);
 
-    $sql = "INSERT INTO pendingUsers (characterID, corporationID, allianceID, groups, authString, active) VALUES ('$characterID','$corporationID','$allianceID','$groups','$authString','$active')";
+    $sql = "INSERT INTO openFleetInvites (fleetId, inviteCode, accessToken) VALUES ('$fleetId','$inviteCode','$accessToken')"
+        . "ON DUPLICATE KEY UPDATE inviteCode='$inviteCode', accessToken='$accessToken'";
 
     if ($conn->query($sql) === TRUE) {
         return null;
     } else {
         return null;
     }
+}
+
+function deleteInviteCode($db, $user, $pass, $dbName, $fleetId)
+{
+    $conn = new mysqli($db, $user, $pass, $dbName);
+    
+    if ($stmt = $conn->prepare("DELETE FROM openFleetInvites WHERE fleetId= ?")) {
+        // Bind the variables to the parameter as strings.
+        $stmt->bind_param("s", $fleetId);
+        // Execute the statement.
+        $stmt->execute();
+        // Close the prepared statement.
+        $stmt->close();
+        return null;
+    }
+    return null;
 }
